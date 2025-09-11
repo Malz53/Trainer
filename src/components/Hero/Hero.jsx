db6450faeee565fdef7b38eae840a132f09af83e
Malz53 <62361229+Malz53@users.noreply.github.com>
2025-09-11 23:43:58 +0200

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="left-h">Left</div>
+        <div className="right-h">Right</div>
+    </div>
+  )
+}
+
+export default Hero;
