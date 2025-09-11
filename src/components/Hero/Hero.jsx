adcd078c99941eedad2f47546618b7329fba625f
Malz53 <johnmalunga53@gmail.com>
2025-09-11 23:54:11 +0200

    `Removed left-h div, added Header component and updated Hero component layout`

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,10 @@ import './Hero.css';
 const Hero = () => {
   return (
     <div className="hero">
-        <div className="left-h">Left</div>
+        <div className="left-h"></div>
+        <Header/>
         <div className="right-h">Right</div>
+
     </div>
   )
 }
